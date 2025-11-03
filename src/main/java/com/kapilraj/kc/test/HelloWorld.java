8c23ff4ace9bba0412590e1c0511def9c06ee2f4
Kapillkc55 <kapillkc55@gmail.com>
2025-11-03 17:33:34 +0545

    first commit

---
+++ src/main/java/com/kapilraj/kc/test/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.kapilraj.kc.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class HelloWorld {
+    @GetMapping("/")
+    public String hello() {
+        return"Hello Sachin, from Kapil.";
+    }
+}
---
+++ src/test/java/com/kapilraj/kc/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kapilraj.kc.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
